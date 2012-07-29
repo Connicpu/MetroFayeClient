dcd57207cd0a9947026dc5fa8ae870ffd053775b
unknown <Connor@.(none)>
2012-07-29 08:45:39 -0700

    I forgot unsubscribe lol. NOW it's done!

--- FayeConnector_Subscription.cs
+++ FayeConnector_Subscription.cs
@@ -8,6 +8,10 @@ namespace MetroFayeClient {
     public partial class FayeConnector {
         private readonly List<string> _subbedChans = new List<string>();
 
+        public bool IsSubscribed(string channel) {
+            return _subbedChans.Contains(channel);
+        }
+
         public async Task<SubscribeResponse> Subscribe(string channel) {
             var guid = Guid.NewGuid();
             var request = new SubscribeRequest {
@@ -20,6 +24,24 @@ namespace MetroFayeClient {
             await Send(request);
             await Helpers.WaitAsync(waiter.WaitHandle);
             var response = await Helpers.DeserializeAsync<SubscribeResponse>(_requestResponses[guid]);
+            if (response.Successful ?? false) _subbedChans.Add(channel);
+            ClearResponse(guid);
+            return response;
+        }
+
+        public async Task<SubscribeResponse> Unsubscribe(string channel) {
+            var guid = Guid.NewGuid();
+            var request = new SubscribeRequest {
+                Id = guid.ToString(),
+                Channel = "/meta/unsubscribe",
+                Subscription = channel,
+            };
+            var waiter = new ManualResetEventSlim();
+            _requestWaiters[guid] = new ManualResetEventSlim();
+            await Send(request);
+            await Helpers.WaitAsync(waiter.WaitHandle);
+            var response = await Helpers.DeserializeAsync<SubscribeResponse>(_requestResponses[guid]);
+            if (response.Successful ?? true) _subbedChans.Remove(channel);
             ClearResponse(guid);
             return response;
         }
